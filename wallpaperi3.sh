aa448b60c4be6331f4bc1b60c5271dd1b135d14e
migueldeoleiros <migueldeoleiros@gmail.com>
2020-11-22 16:06:08 +0100

    changed name of directories

--- wallpaperi3.sh
+++ wallpaperi3.sh
@@ -2,12 +2,12 @@
 #
 #migueldeoleiros: September 2020
 #
-#This is a simple script to change the Wallpaper on my i3 config through a rofi menu
+#This is a simple script to change the wallpaper on my i3 config through a rofi menu
 #It requires to restart i3 to see the changes
 #
 set -euo pipefail
 
-NewWallName=$(ls ~/Wallpapers | rofi -dmenu -i -p)
-NewWall='~/Wallpapers/'$NewWallName''
+NewWallName=$(ls ~/wallpapers | rofi -dmenu -i -p)
+NewWall='~/wallpapers/'$NewWallName''
 OldWall=$(grep 'exec_always feh' ~/.config/i3/config | awk '{print $4}')
 sed -i 's#'$OldWall'#'$NewWall'#g' ~/.config/i3/config
