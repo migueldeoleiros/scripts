781a6d97ad82d6e944068d9e9bc576d9147ed0bb
migueldeoleiros <migueldeoleiros@gmail.com>
2021-03-11 13:02:40 +0100

    fixed bug

--- nvidiarun.sh
+++ nvidiarun.sh
@@ -5,7 +5,7 @@
 pro='tty2nvidia-xrun'
 var=$(ps -u $USER | awk '!seen[$4]++ {print $2 $4}' | grep $pro)
 
-if [[ "$(echo $var)" != "$(echo $pro)" ]]
+if [[ "$(echo $var)" == "$(echo $pro)" ]]
     then
     xrandr --output HDMI-1-2 --off
 fi
