afdff191752e9669e8379f82be57bda388b655b5
migueldeoleiros <migueldeoleiros@gmail.com>
2022-01-13 21:10:06 +0100

    added url to oi

--- rofiSearch.sh
+++ rofiSearch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-#simple script to run Oi assistant on rofi
+# Simple script to run Oi assistant on rofi
+# find Oi in https://github.com/PureArtistry/oi/
 
 search=$(rofi -dmenu -p "Search")
 output=$(oi $search)
